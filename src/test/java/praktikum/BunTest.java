9ab53488189739b24f6a8a27eaf7806538c6d7d5
alekulikov <aleksandr.a.kulikov@mvideo.ru>
2022-04-29 12:59:16 +0300

    Add BunTest, IngredientTest, IngredientTypeTest

--- src/test/java/praktikum/BunTest.java
+++ src/test/java/praktikum/BunTest.java
@@ -1,4 +1,20 @@
 package praktikum;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class BunTest {
+
+    @Test
+    public void getNameReturnCorrectValue() {
+        Bun bun = new Bun("big bun", 123.5f);
+        Assert.assertEquals("return incorrect value", "big bun", bun.getName());
+    }
+
+    @Test
+    public void getPriceReturnCorrectValue() {
+        Bun bun = new Bun("big bun", 123.5f);
+        Assert.assertEquals("return incorrect value", 123.5f, bun.getPrice(), 0.0f);
+    }
+
 }
--- src/test/java/praktikum/IngredientTest.java
+++ src/test/java/praktikum/IngredientTest.java
@@ -1,4 +1,20 @@
 package praktikum;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class IngredientTest {
+
+    @Test
+    public void getNameReturnCorrectValue() {
+        Ingredient ingredient = new Ingredient(IngredientType.SAUCE, "ketchup", 50.5f);
+        Assert.assertEquals("return incorrect value", "ketchup", ingredient.getName());
+    }
+
+    @Test
+    public void getPriceReturnCorrectValue() {
+        Ingredient ingredient = new Ingredient(IngredientType.SAUCE, "ketchup", 50.5f);
+        Assert.assertEquals("return incorrect value", 50.5f, ingredient.getPrice(), 0.0f);
+    }
+
 }
